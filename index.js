5bc7b68077a9544ab2233c8014653101af829dfb
Abdul Azeez <103828615+kyrios12@users.noreply.github.com>
2023-07-24 05:28:24 +0530

    Setting up server and installing express

---
+++ index.js
@@ -0,0 +1,18 @@
+// importing required packages
+const express = require('express');
+
+
+
+
+
+
+
+
+
+
+
+
+// starting the server
+app.listen(3000, function(){
+    console.log('API is live on http://localhost:3000/products');
+});
