214f13c20cc68e54fc84b15ebab80cdff933b86c
Abdul Azeez <103828615+kyrios12@users.noreply.github.com>
2023-07-24 05:55:50 +0530

    Creating function for performing delete operation using product id

--- controllers/products_controller.js
+++ controllers/products_controller.js
@@ -30,4 +30,16 @@ module.exports.create = async function(req, res) {
     res.send(error);
   }
 };
-    
+//To perform delete operation
+module.exports.delete = async function(req, res) {
+    const productID = req.params.productID; // Replace this with the actual product ID to delete
+  
+    try {
+      const deleteOneAsync = promisify(Product.deleteOne.bind(Product));
+      await deleteOneAsync({_id: productID});
+      res.send({ message: "Product deleted" });
+    } catch (error) {
+      res.send(error);
+    }
+};
+
--- routes/products.js
+++ routes/products.js
@@ -3,9 +3,13 @@ const router = express.Router();
 
 // initializing products controller
 const productsController = require('../controllers/products_controller');
-//To create a product
-router.post('/create', productsController.create);
+
 // to get all the products
 router.get('/', productsController.products);
 
+//To create a product
+router.post('/create', productsController.create);
+
+// to delete a product using it's ID
+router.delete('/:productID', productsController.delete);
 module.exports = router;
